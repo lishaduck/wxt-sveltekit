b1aeed47b286444d63a72677f29edd0aed38d154
Eli <88557639+lishaduck@users.noreply.github.com>
2025-10-21 14:57:53 -0500

    wip

---
+++ svelte.config.js
@@ -0,0 +1,31 @@
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+  // Consult https://svelte.dev/docs/kit/integrations
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+
+  kit: {
+    adapter: adapter({
+      fallback: "404.html",
+    }),
+
+    output: {
+      bundleStrategy: "inline",
+    },
+    router: {
+      resolution: "client",
+      type: "hash",
+    },
+  },
+
+  compilerOptions: {
+    experimental: {
+      async: true,
+    },
+  },
+};
+
+export default config;
