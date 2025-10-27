38b4c9ebe403c013f7fe0d89037499f91dcf770d
Eli <88557639+lishaduck@users.noreply.github.com>
2025-10-26 20:32:45 -0500

    feat: working!

--- modules/sveltekit-builder.ts
+++ modules/sveltekit-builder.ts
@@ -1,10 +1,11 @@
-import { sveltekit } from "@sveltejs/kit/vite";
-import tailwindcss from "@tailwindcss/vite";
+import fs from "node:fs/promises";
 import { join, resolve } from "node:path";
 import { type InlineConfig, build, mergeConfig } from "vite";
-import devtoolsJson from "vite-plugin-devtools-json";
 import type { PopupEntrypoint } from "wxt";
 import { defineWxtModule } from "wxt/modules";
+
+const HEAD_REGEX = /\n\t\t<link .+>.+<\/head>/s;
+const BODY_REGEX = /<body.*>.+<\/body>/s;
 
 export default defineWxtModule((wxt) => {
   let baseViteConfig: InlineConfig;
@@ -14,18 +15,51 @@ export default defineWxtModule((wxt) => {
 
   const buildPopup = async () => {
     wxt.logger.info("`[sveltekit-builder]` Building SvelteKit project...");
-    const prebuildConfig: InlineConfig = {
-      plugins: [tailwindcss(), sveltekit(), devtoolsJson()],
-    };
+    process.env["WXT_SVELTEKIT_OUTDIR"] = join(wxt.config.outDir, "popup");
+
+    const prebuildConfig = await import("../vite.config.js");
+
     const {
       build: _,
       publicDir: __,
       ...finalConfig
-    } = mergeConfig(baseViteConfig, prebuildConfig);
-
-    process.env["WXT_SVELTEKIT_OUTDIR"] = join(wxt.config.outDir, "popup");
+    } = mergeConfig(baseViteConfig, prebuildConfig.default);
 
     await build(finalConfig);
+
+    const popupHtmlPath = join(wxt.config.outDir, "popup.html");
+    const popupDirPath = join(wxt.config.outDir, "popup");
+    const indexHtmlPath = join(popupDirPath, "index.html");
+
+    const [popupHtml, indexHtml] = await Promise.all([
+      fs.readFile(popupHtmlPath, { encoding: "utf-8" }),
+      fs.readFile(indexHtmlPath, { encoding: "utf-8" }),
+    ]);
+
+    const svelteKitHead = indexHtml.match(HEAD_REGEX)?.[0];
+    const svelteKitBody = indexHtml.match(BODY_REGEX)?.[0];
+
+    if (!svelteKitHead || !svelteKitBody) {
+      wxt.logger.error(
+        "`[sveltekit-builder]` Could not find tags in SvelteKit popup index.html",
+      );
+      return;
+    }
+
+    const popupHtmlModified = popupHtml
+      .replace("</head>", svelteKitHead)
+      .replace("<body></body>", svelteKitBody);
+
+    await Promise.all([
+      fs.writeFile(popupHtmlPath, popupHtmlModified, { encoding: "utf-8" }),
+
+      fs.rename(
+        join(wxt.config.outDir, "popup", "_app"),
+        join(wxt.config.outDir, "_app"),
+      ),
+    ]);
+    await fs.rm(popupDirPath, { recursive: true, force: true });
+
     wxt.logger.success("`[sveltekit-builder]` Done!");
   };
 
