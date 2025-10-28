16c59cea627b684eef0a0157dd4818d044eb477b
Eli <88557639+lishaduck@users.noreply.github.com>
2025-10-27 20:58:22 -0500

    chore: write a slightly better demo

--- example/src/routes/+page.svelte
+++ example/src/routes/+page.svelte
@@ -1,5 +1,17 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-  documentation
-</p>
+<main class="p-2">
+  <h1 class="text-4xl text-pretty">Hello from SvelteKit!</h1>
+  <p>
+    Visit <a class="underline" href="https://svelte.dev/docs/kit"
+      >svelte.dev/docs/kit</a
+    >
+    to read the SvelteKit documentation, or visit
+    <a class="underline" href="https://wxt.dev/guide/introduction.html"
+      >wxt.dev/guide/introduction</a
+    > to read the WXT documentation.
+  </p>
+
+  <a
+    class="block w-min rounded-2xl border-2 bg-green-100 p-1 text-2xl dark:bg-green-900"
+    href="#/counter">Demo</a
+  >
+</main>
---
+++ example/src/routes/counter/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  let number = $state(0);
+</script>
+
+<main class="flex h-full items-center p-2">
+  <button
+    class="inline-block w-min grow rounded-2xl border-2 bg-green-100 p-1 text-2xl dark:bg-green-900"
+    onclick={() => number--}>-</button
+  >
+  <span class="grow p-1 text-center text-4xl">{number}</span>
+  <button
+    class="inline-block w-min grow rounded-2xl border-2 bg-green-100 p-1 text-2xl dark:bg-green-900"
+    onclick={() => number++}>+</button
+  >
+</main>
